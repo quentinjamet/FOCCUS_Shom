085802caf16bd6920f76b7381b9dfee126de95c0
Quentin Jamet <quentin.jamet@shom.fr>
2025-05-20 09:19:49 +0000

    update names

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='FOCCUS-Shom',
+setup(name='FOCCUS_Shom',
       version='0.0.1',
       description='Routines used in FOCCUS project at Shom',
-      url='https://github.com/quentinjamet/FOCCUS-Shom',
+      url='https://github.com/quentinjamet/FOCCUS_Shom',
       author='qjamet',
       author_email='quentin.jamet@shom.fr',
       license='MIT',
-      packages=['FOCCUS-Shom'],
+      packages=['FOCCUS_Shom'],
       install_requires=['numpy', 'scipy', 'xarray', 'datetime'],
       zip_safe=False)
